35abdc4afcc6426e9c1e1e0355ccdc7ded36da83
Nimi <ttu91@hotmail.com>
2021-12-07 19:37:07 +0200

    valmis projekti

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -16,7 +16,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        bestTime = PlayerPrefs.GetFloat("bestTime", 100000);
+        bestTime = PlayerPrefs.GetFloat("bestTime", 99999);
     }
     // Update is called once per frame
     void Update()
